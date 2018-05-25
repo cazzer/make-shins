d196df14102c1244ec8006e9df420d04a8e2f741
Caleb Brewer <caleb@cazzer.io>
2018-05-25 14:08:00 -0700

    updating fs-extra, promisifying

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import fs from 'fs-extra'
 import path from 'path'
 import shins from 'shins'
 import npmRoot from 'npm-root'
+import { promisify } from 'es6-promisify'
 
 const d = new Debug('make-shins')
 
+const npmRootAsync = promisify(npmRoot)
+const shinsRenderAsync = promisify(shins.render).bind(shins)
 
 export default async function makeShins(options) {
   console.time('make-shins')
@@ -13,57 +16,51 @@ export default async function makeShins(options) {
   const markdownString = fs.readFileSync(path.resolve(options.input), 'utf8')
 
   d('Rendering shins')
-  shins.render(
+  const html = shinsRenderAsync(
     markdownString,
     {
       customCss: options.customCss,
       inline: options.inline,
       minify: options.minify
-    },
-    (error, html) => {
-      if (error) {
-        console.error('Could not render shins', error)
-      }
-
-      if (options.output) {
-        d('Preparing output directory')
-        // prepare by removing, since shins uses symlinks which break copySync
-        fs.removeSync(options.output)
-        d('Writing output')
-        npmRoot({
-          global: !options.local
-        }, (error, rootPath) => {
-          fs.copySync(
-            path.join(
-              rootPath,
-              '/make-shins/node_modules/shins'
-            ),
-            options.output,
-            {
-              dereference: true
-            }
-          )
-          fs.writeFileSync(path.join(options.output, 'index.html'), html)
-
-          if (options.customCss) {
-            d('Copying custom CSS')
-            fs.copySync(
-              options.customCss,
-              path.join(options.output, '/pub/css')
-            )
-          }
-
-          if (options.logo) {
-            d('Copying logo.png')
-            fs.copySync(
-              options.logo,
-              path.join(options.output, 'source/images/logo.png')
-            )
-          }
-        })
-      }
-      d('Finished')
-      console.timeEnd('make-shins')
     }
   )
+
+  if (options.output) {
+    d('Preparing output directory')
+    // prepare by removing, since shins uses symlinks which break copySync
+    fs.removeSync(options.output)
+    d('Writing output')
+    const rootPath = await npmRootAsync({
+      global: !options.local
+    })
+    await fs.copy(
+      path.join(
+        rootPath,
+        '/make-shins/node_modules/shins'
+      ),
+      options.output,
+      {
+        dereference: true
+      }
+    )
+    await fs.writeFile(path.join(options.output, 'index.html'), html)
+
+    if (options.customCss) {
+      d('Copying custom CSS')
+      await fs.copy(
+        options.customCss,
+        path.join(options.output, '/pub/css')
+      )
+    }
+
+    if (options.logo) {
+      d('Copying logo.png')
+      await fs.copy(
+        options.logo,
+        path.join(options.output, 'source/images/logo.png')
+      )
+    }
+  }
+  d('Finished')
+  console.timeEnd('make-shins')
 }
